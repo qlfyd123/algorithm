9e91ac97d002f1228c6b0794d1c0283680db5a28
qlfyd123 <hajewoong@gmail.com>
2022-06-27 10:29:58 +0900

    20220627

--- src/baekjoon/baekjoon_14469.java
+++ src/baekjoon/baekjoon_14469.java
@@ -1,5 +1,10 @@
 package baekjoon;
 
+
+/*
+* 소가 도착하는 시간과 검문에 소요되는 시간이 주어질때
+* 모든 소가 검문을 받고 농장에 입장할때까지 걸리는 시간을 출력
+* */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
