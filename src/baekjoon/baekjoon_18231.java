5fd781c58ac386b0f3e25861bc8c8717ec7af343
qlfyd123 <hajewoong@gmail.com>
2022-09-01 14:00:48 +0900

    20220901

--- src/baekjoon/baekjoon_18231.java
+++ src/baekjoon/baekjoon_18231.java
@@ -7,13 +7,19 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
-public class Main {
+public class baekjoon_18231 {
     static HashMap<Integer, Set<Integer>> cityMap = new HashMap<>();
     static Set<Integer> destroyedCity = new HashSet<>();
     static Set<Integer> destroyedCityInCase = new HashSet<>();
     static Set<Integer> answer = new HashSet<>();
 
+    /**
+     * @param left stringTokenizer의 첫번째 값
+     * @param right stringTokenizer의 두번째 값
+     * @return left, right 값을 초기값으로 하는 HashSet를 리턴
+     */
     public static Set<Integer> newCitySet(int left, int right) {
         Set<Integer> set = new HashSet<>();
         set.add(left);
@@ -22,6 +28,9 @@ public class Main {
     }
 
 
+    /**
+     * stringTokenizer로 입력받은 연결된 도시들을 left값과 right값을 키값으로 하는 cityMap에 저장합니다
+     */
     public static void insertIntoCitySet(StringTokenizer st) {
         int left = Integer.parseInt(st.nextToken());
         int right = Integer.parseInt(st.nextToken());
@@ -39,6 +48,10 @@ public class Main {
         }
     }
 
+    /**
+     * @param city cityMap key value
+     * @return city를 키값으로 하는 Set의 값 중에서 destroyedCity에 존재하지 않는 값이 있을경우 false를 리턴하고 그렇지 않을경우 true를 리턴합니다
+     */
     public static boolean isInRange(int city) {
         Set<Integer> set = cityMap.get(city);
         for (int i : set) {
@@ -49,8 +62,12 @@ public class Main {
         return true;
     }
 
-    public static void boom(Set<Integer> city) {
-        destroyedCityInCase.addAll(city);
+    /**
+     * @param city cityMap key value
+     */
+    public static void boom(int city) {
+        Set<Integer> set = cityMap.get(city);
+        destroyedCityInCase.addAll(set);
     }
 
     public static void main(String[] args) throws IOException {
@@ -75,19 +92,25 @@ public class Main {
 
         for (int city : cityMap.keySet()) {
             if (isInRange(city)) {
-                boom(cityMap.get(city));
+                boom(city);
                 answer.add(city);
             }
         }
 
-        if (!destroyedCity.containsAll(destroyedCityInCase)) {
-            System.out.println(-1);
-            return;
-        }
-        System.out.println(answer.size());
-        StringBuilder sb = new StringBuilder();
-        answer.forEach(e -> sb.append(e).append(" "));
+        destroyedCity.stream()
+                .filter(e -> !cityMap.containsKey(e))
+                .forEach(e -> {
+                    answer.add(e);
+                    destroyedCityInCase.add(e);
+                });
 
-        System.out.println(sb.toString().trim());
+        if (destroyedCityInCase.containsAll(destroyedCity)) {
+            System.out.println(answer.size());
+            StringBuilder sb = new StringBuilder();
+            answer.stream().collect(Collectors.toList()).stream().sorted().forEach(e -> sb.append(e).append(" "));
+            System.out.println(sb.toString().trim());
+        } else {
+            System.out.println(-1);
+        }
     }
 }
