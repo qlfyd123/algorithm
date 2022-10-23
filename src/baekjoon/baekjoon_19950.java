9178157124d4f065281ae1831d38b62a2e7dad12
qlfyd123 <hajewoong@gmail.com>
2022-10-23 17:59:15 +0900

    3차원 막대기 연결하기

--- src/baekjoon/baekjoon_19950.java
+++ src/baekjoon/baekjoon_19950.java
@@ -6,13 +6,16 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class Main {
+/*
+ * https://www.acmicpc.net/problem/19950
+ * */
+public class baekjoon_19950 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] location = br.readLine().split(" ");
         double distanceBetween;
         distanceBetween = Math.sqrt(IntStream.range(0, 3)
-                .map(value -> (int) Math.pow(Integer.parseInt(location[5 - value]) - Integer.parseInt(location[value]), 2))
+                .map(value -> (int) Math.pow(Integer.parseInt(location[value + 3]) - Integer.parseInt(location[value]), 2))
                 .sum());
         int N = Integer.parseInt(br.readLine());
 
@@ -22,13 +25,18 @@ public class Main {
                 .toArray();
 
         double stickLengthSum = 0;
-        for (int stick : sticks)
-            stickLengthSum += stick;
+        for (int i = 0; i < N - 1; i++)
+            stickLengthSum += sticks[i];
 
-        if (stickLengthSum < distanceBetween)
+        int maxStickLength = sticks[N - 1];
+        if (stickLengthSum + maxStickLength < distanceBetween)
             System.out.println("NO");
-        else if (stickLengthSum - sticks[N - 1] + distanceBetween < sticks[N - 1])
-            System.out.println("NO");
-        else System.out.println("YES");
+        else if (stickLengthSum + maxStickLength == distanceBetween)
+            System.out.println("YES");
+        else {
+            if (maxStickLength - stickLengthSum <= distanceBetween)
+                System.out.println("YES");
+            else System.out.println("NO");
+        }
     }
 }
