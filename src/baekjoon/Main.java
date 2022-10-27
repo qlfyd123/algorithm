75c1cf07c7f710032fcc3f841166c05a67e104a7
qlfyd123 <hajewoong@gmail.com>
2022-10-27 22:19:11 +0900

    최단경로

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -6,15 +6,14 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int V;
-    static int E;
-    static int K;
-    static Map<Integer, Node> nodeMap = new HashMap<>();
+    static int V, E, K;
+    static List<ArrayList<Edge>> graph;
+    static int[] cost;
+    static Queue<Edge> openNode = new PriorityQueue<>(Comparator.comparingInt(o -> o.weight));
     static boolean[] closedNode;
 
     static class Edge {
-        int end;
-        int weight;
+        int end, weight;
 
         public Edge(int end, int weight) {
             this.end = end;
@@ -22,25 +21,35 @@ public class Main {
         }
     }
 
-    static class Node implements Comparable<Node> {
-        int location;
-        int cost = Integer.MAX_VALUE;
-        List<Edge> edgeList = new ArrayList<>();
-
-        public Node(int location) {
-            this.location = location;
-        }
-
-        public void addEdge(Edge edge) {
-            edgeList.add(edge);
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.cost - o.cost;
-        }
+    public static void addToNode(String edgeValues) {
+        StringTokenizer st = new StringTokenizer(edgeValues);
+        int u = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        graph.get(u).add(new Edge(v, w));
     }
 
+    public static void dijkstra() {
+        int curLocation;
+        cost[K] = 0;
+        openNode.add(new Edge(K, 0));
+        for (int i = 0; i < V & !openNode.isEmpty(); ) {
+            curLocation = openNode.poll().end;
+            if (closedNode[curLocation]) {
+                continue;
+            }
+            ArrayList<Edge> edgeList = graph.get(curLocation);
+            int currNodeCost = cost[curLocation];
+            for (Edge edge : edgeList) {
+                if (edge.weight + currNodeCost < cost[edge.end]) {
+                    cost[edge.end] = edge.weight + currNodeCost;
+                    openNode.add(edge);
+                }
+            }
+            closedNode[curLocation] = true;
+            i++;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -48,42 +57,27 @@ public class Main {
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(br.readLine());
-        for (int i = 0; i < V; i++) {
-            nodeMap.put(i + 1, new Node(i + 1));
-        }
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            nodeMap.get(u).addEdge(new Edge(v, w));
-        }
+        graph = new ArrayList<>() {
+            {
+                for (int i = 0; i < V + 1; i++) {
+                    add(i, new ArrayList<>());
+                }
+            }
+        };
+        for (int i = 0; i < E; i++)
+            addToNode(br.readLine());
+        cost = new int[V + 1];
+        Arrays.fill(cost, Integer.MAX_VALUE);
         closedNode = new boolean[V + 1];
         Arrays.fill(closedNode, false);
-        nodeMap.get(K).cost = 0;
-        closedNode[K] = true;
-        Queue<Node> openNode = new PriorityQueue<>();
-        openNode.add(nodeMap.get(K));
-        for (int i = 0; i < V & !openNode.isEmpty(); i++) {
-            Node currentNode = openNode.poll();
-            if (closedNode[currentNode.location]) {
-                i--;
-                continue;
-            }
-            for (Edge neighbor : currentNode.edgeList) {
-                Node neighborNode = nodeMap.get(neighbor.end);
-                neighborNode.cost = Math.min(neighborNode.cost, neighbor.weight + currentNode.cost);
-                openNode.add(neighborNode);
-            }
-            closedNode[currentNode.location] = true;
-        }
-        for (Node node : nodeMap.values()) {
-            int cost = node.cost;
-            if (cost != Integer.MAX_VALUE) {
-                System.out.println(cost);
-            } else {
+
+        dijkstra();
+        for (int i = 1; i < V + 1; i++) {
+            int ans = cost[i];
+            if (ans == Integer.MAX_VALUE)
                 System.out.println("INF");
-            }
+            else
+                System.out.println(cost[i]);
         }
     }
 }
