c11d0631af465b903a131594b68780748aa0cd1c
qlfyd123 <hajewoong@gmail.com>
2023-01-08 18:03:05 +0900

    바이러스

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,20 +3,23 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-class virus {
+// TODO: 2023-01-08 벽세우는 함수 마무리,lab배열 클론처리
+class Virus {
     final private int[][] lab;
-    private ArrayList<int[]> wall;
+    private int[][] labClone;
+    private Stack<int[]> wall;
     final private int row, column;
     private int maxSafeZone = 0;
 
-    public virus(int row, int column) {
+    public Virus(int row, int column) {
         this.row = row;
         this.column = column;
         lab = new int[row][column];
     }
+
 
     public void getLabStructure(BufferedReader br) throws IOException {
         for (int i = 0; i < row; i++) {
@@ -27,34 +30,69 @@ class virus {
         }
     }
 
-    public void makeWall(int row, int column) {
-        wall.add(new int[]{row, column});
-        lab[row][column] = 1;
-        if (wall.size() == 3) {
-            calcSafeZone();
-        } else {
-            for (int i = row; i < this.row; ) {
-                for (int j = column + 1; j < this.column; j++) {
-                    if (lab[row][column] == 0) {
-                        makeWall(i, j);
-                        return;
-                    }
+    public int ans() {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (lab[i][j] == 0) {
+                    labClone = lab.clone();
+                    makeWall(i, j);
                 }
-                i++;
             }
         }
     }
 
-    public int calcSafeZone() {
-        for(int i=0;i<row;i++){
-            for(int j=0;j<column;j++){
-                if()
-            }
+    private int[][] makeWall(int row, int column) {
+        labClone[row][column] = 1;
+        if (wall.size() == 2) {
+            calcSafeZone(labClone);
+        } else {
+            wall.add(new int[]{row, column});
         }
+        for (int i = row; i < this.row; ) {
+            for (int j = column + 1; j < this.column; j++) {
+                if (labClone[row][column] == 0) {
+                    if ()
+                        makeWall(i, j, labClone);
+                    return lab;
+                }
+            }
+            i++;
+        }
+        return lab;
     }
 
-    private void infection(){
+    private void calcSafeZone(int[][] labClone) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (labClone[i][j] == 2) {
+                    infection(row, column);
+                }
+            }
+        }
 
+        int safeZone = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (lab[i][j] == 0) {
+                    safeZone++;
+                }
+            }
+        }
+        maxSafeZone = Math.max(safeZone, maxSafeZone);
+    }
+
+    private void infection(int row, int column) {
+        int[][] direction = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int[][] labClone = lab.clone();
+        for (int i = 0; i < 4; i++) {
+            try {
+                if ((labClone[row + direction[i][0]][column + direction[i][1]] == 0)) {
+                    labClone[row + direction[i][0]][column + direction[i][1]] = 2;
+                    infection(row + direction[i][0], column + direction[i][1]);
+                }
+            } catch (IndexOutOfBoundsException ignored) {
+            }
+        }
     }
 }
 
@@ -65,5 +103,7 @@ public class Main {
         int row = Integer.parseInt(st.nextToken());
         int column = Integer.parseInt(st.nextToken());
 
+        Virus virus = new Virus(row, column);
+        virus.getLabStructure(br);
     }
 }
