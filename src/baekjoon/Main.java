75415a065c8faaf29c6449778ccb4101ff23154b
qlfyd123 <hajewoong@gmail.com>
2023-01-16 17:17:12 +0900

    구슬 탈출2

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -6,6 +6,20 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
+// TODO: 2023-01-16 아래 테스트 케이스 맞춰서 수정 
+/*
+10 10
+##########
+#.#....###
+#........#
+#........#
+##B..#...#
+#.#......#
+#.#..R...#
+#...O#...#
+#.#....###
+##########
+* */
 class Block {
     int row, column;
 
@@ -35,60 +49,78 @@ class Board {
     final private int row, column;
     final private Block hole;
     int count = Integer.MAX_VALUE;
+    boolean[][][] visited;
 
     public Board(String[][] board, int row, int column, Block hole) {
         this.board = board;
         this.row = row;
         this.column = column;
         this.hole = hole;
+        visited = new boolean[2][row][column];
     }
 
-    public void moveBall(Ball[] balls, Stack<Ball[]> stack) {
+    public void moveBall(Stack<Ball[]> stack) {
+        Ball[] balls = stack.pop();
         Ball red = balls[0];
         Ball blue = balls[1];
+        //상,하,좌,우 순서
         for (int i = 0; i < 4; i++) {
-            Block nextRedBall = getNextBallLocation(red, i);
-            Block nextBlueBall = getNextBallLocation(blue, i);
+            Ball nextRedBall = getNextBallLocation(red, i);
+            Ball nextBlueBall = getNextBallLocation(blue, i);
             boolean isSameLocation = nextRedBall.row == nextBlueBall.row & nextRedBall.column == nextBlueBall.column;
             boolean isRedHole = isHoleIn(red, nextRedBall);
             boolean isBlueHole = isHoleIn(blue, nextBlueBall);
             if (!isBlueHole) {
                 if (isRedHole) {
-                    count = Math.min(red.count, count);
+                    count = Math.min(nextRedBall.count, count);
                 } else {
                     if (isSameLocation) {
                         int redBallMoveDistance = Math.abs(nextRedBall.row - red.row + nextRedBall.column - red.column);
                         int blueBallMoveDistance = Math.abs(nextRedBall.row - blue.row + nextRedBall.column - blue.column);
+                        int[] dx = {-1, 1, 0, 0};
+                        int[] dy = {0, 0, -1, 1};
                         if (redBallMoveDistance > blueBallMoveDistance) {
-                            int[] dx = {1, -1, 0, 0};
-                            int[] dy = {0, 0, -1, 1};
                             nextRedBall.row -= dx[i];
                             nextRedBall.column -= dy[i];
+                        } else {
+                            nextBlueBall.row -= dx[i];
+                            nextBlueBall.column -= dy[i];
                         }
-                        stack.push(new Ball[]{new Ball(nextRedBall, red.count + 1), new Ball(nextBlueBall, blue.count + 1)});
                     }
+                    if (visited[0][nextRedBall.row][nextRedBall.column] & visited[1][nextBlueBall.row][nextBlueBall.column]) {
+                        continue;
+                    } else {
+                        visited[0][nextRedBall.row][nextRedBall.column] = true;
+                        visited[1][nextBlueBall.row][nextBlueBall.column] = true;
+                    }
+                    stack.push(new Ball[]{nextRedBall, nextBlueBall});
                 }
             }
         }
     }
 
     private boolean isHoleIn(Ball pre, Block next) {
+
         if (pre.row == hole.row) {
-            return hole.column > pre.column & hole.column <= next.column;
+            int big = Math.max(pre.column, next.column);
+            int small = Math.min(pre.column, next.column);
+            return hole.column >= small & hole.column <= big;
         } else if (pre.column == hole.column) {
-            return hole.row > pre.row & hole.row <= next.row;
+            int big = Math.max(pre.row, next.row);
+            int small = Math.min(pre.row, next.row);
+            return hole.row >= small & hole.row <= big;
         } else return false;
     }
 
-    private Block getNextBallLocation(Ball red, int i) {
-        int[] dx = {1, -1, 0, 0};
+    private Ball getNextBallLocation(Ball ball, int i) {
+        int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
         int distance = 1;
-        while (!board[red.row + dx[i] * distance][red.column + dy[i] * distance].equals("#")) {
+        while (!board[ball.row + dx[i] * distance][ball.column + dy[i] * distance].equals("#")) {
             distance++;
         }
-        Block nextBallLocation = new Block(red.row + dx[i] * distance, red.column + dy[i] * distance);
-        return nextBallLocation;
+        distance--;
+        return new Ball(ball.row + dx[i] * distance, ball.column + dy[i] * distance, ball.count + 1);
     }
 }
 
@@ -110,6 +142,7 @@ public class Main {
                     red = new Ball(i, j, 0);
                     board[i][j] = ",";
                 } else if (board[i][j].equals("B")) {
+                    blue = new Ball(i, j, 0);
                     board[i][j] = ",";
                 } else if (board[i][j].equals("O")) {
                     hole = new Block(i, j);
@@ -119,8 +152,12 @@ public class Main {
         Board toy = new Board(board, row, column, hole);
         Stack<Ball[]> stack = new Stack<>();
         stack.push(new Ball[]{red, blue});
+        toy.visited[0][red.row][red.column] = true;
+        toy.visited[1][blue.row][blue.column] = true;
         while (!stack.isEmpty()) {
-
+            toy.moveBall(stack);
         }
+
+        System.out.println(toy.count == Integer.MAX_VALUE ? -1 : toy.count);
     }
 }
