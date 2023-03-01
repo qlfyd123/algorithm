e909d6d7a08c02c055bd41d4772ec49ee4118ac0
qlfyd123 <hajewoong@gmail.com>
2023-03-01 21:31:52 +0900

    치킨 배달

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -4,62 +4,67 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
     static int totalDistance = Integer.MAX_VALUE;
+    static boolean[] visited;
+    static List<int[]> chickens = new ArrayList<>();
+    static List<int[]> houses = new ArrayList<>();
+    static int[][] roadMap;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        int[][] cityMap = new int[N][N];
-        ArrayList<int[]> chickens = new ArrayList<>();
-        List<int[]> houses = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                cityMap[i][j] = Integer.parseInt(st.nextToken());
-                if (cityMap[i][j] == 2) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num == 2) {
                     chickens.add(new int[]{i, j, 0});
-                } else if (cityMap[i][j] == 1) {
+                } else if (num == 1) {
                     houses.add(new int[]{i, j});
                 }
             }
         }
-
-        List<int[]> selectedChicken = new LinkedList<>();
-        for (int[] chicken : chickens) {
-            selectedChicken.add(chicken);
-            selectChicken(selectedChicken, chickens, houses);
-            selectedChicken.remove(0);
+        roadMap = new int[houses.size()][chickens.size()];
+        for (int i = 0; i < houses.size(); i++) {
+            int[] house = houses.get(i);
+            for (int j = 0; j < chickens.size(); j++) {
+                int[] chicken = chickens.get(j);
+                roadMap[i][j] = Math.abs(house[0] - chicken[0]) + Math.abs(house[1] - chicken[1]);
+            }
         }
+        visited = new boolean[chickens.size()];
+        combination(0, 0);
         System.out.println(totalDistance);
     }
 
-    public static void selectChicken(List<int[]> selectedChicken, ArrayList<int[]> chickens, List<int[]> houses) {
-        if (selectedChicken.size() == M) {
-            int totalDis = 0;
-            for (int[] house : houses) {
+    public static void combination(int combinationSize, int index) {
+        if (combinationSize == M) {
+            int totalCickenRoad = 0;
+            for (int i = 0, housesSize = houses.size(); i < housesSize; i++) {
                 int chickenRoad = Integer.MAX_VALUE;
-                for (int[] chicken : selectedChicken) {
-                    chickenRoad = Math.min(chickenRoad, Math.abs(house[0] - chicken[0]) + Math.abs(house[1] - chicken[1]));
+                for (int j = 0; j < visited.length; j++) {
+                    if (visited[j]) {
+                        chickenRoad = Math.min(chickenRoad, roadMap[i][j]);
+                    }
                 }
-                totalDis += chickenRoad;
+                totalCickenRoad += chickenRoad;
             }
-            totalDistance = Math.min(totalDis, totalDistance);
-            selectedChicken.remove(M - 1);
+            totalDistance = Math.min(totalCickenRoad, totalDistance);
         } else {
-            for (int[] chicken : chickens) {
-                selectedChicken.add(chicken);
-                selectChicken(selectedChicken, chickens, houses);
+            for (int i = index; i < visited.length; i++) {
+                if (visited[i]) continue;
+                visited[i] = true;
+                combination(combinationSize + 1, index);
+                visited[i] = false;
             }
-            selectedChicken.remove(selectedChicken.size() - 1);
         }
     }
 }
