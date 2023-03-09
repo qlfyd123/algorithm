34546345a5c609a35479cd00298e0107eeafc4a0
qlfyd123 <hajewoong@gmail.com>
2023-03-09 16:53:07 +0900

    최소 스패닝 트리

---
+++ src/baekjoon/Main.java
@@ -0,0 +1,83 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        Queue<Edge> edges = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
+        while (E-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(start, end, cost));
+        }
+        int[] union = new int[V + 1];
+        for (int i = 1; i < V + 1; i++) {
+            union[i] = i;
+        }
+        int spanningTree = 0;
+        while (!edges.isEmpty()) {
+            Edge edge = edges.poll();
+            if (!isCycle(edge.start, edge.end, union)) {
+                union(edge.start, edge.end, union);
+                spanningTree += edge.cost;
+            }
+        }
+        System.out.println(spanningTree);
+    }
+
+    /**
+     * @param x     그래프에 속한 원소
+     * @param union 원소들의 집합을 표현한 배열
+     * @return 임의의 원소 x가 속한 집합의 번호를 리턴합니다
+     */
+    public static int getRoot(int x, int[] union) {
+        while (x != union[x]) {
+            x = union[x];
+        }
+        return x;
+    }
+
+    /**
+     * 두 원소 x,y가 속한 부분집합을 합칩니다.
+     *
+     * @param x     그래프에 포함되는 임의의 원소 x
+     * @param y     그래프에 포함되는 임의의 원소 y
+     * @param union 원소들의 집합을 표현한 배열
+     */
+    public static void union(int x, int y, int[] union) {
+        union[y] = getRoot(x, union);
+    }
+
+    /**
+     * @param x     그래프에 포함되는 임의의 원소 x
+     * @param y     그래프에 포함되는 임의의 원소 y
+     * @param union 원소들의 집합을 표현한 배열
+     * @return 임의의 두 원소 x,y가 같은 부분집합에 속해있는지 반환합니다.
+     */
+    public static boolean isCycle(int x, int y, int[] union) {
+        return getRoot(x, union) == getRoot(y, union);
+    }
+
+
+    private static class Edge {
+        int end, cost, start;
+
+        public Edge(int start, int end, int cost) {
+            this.end = end;
+            this.cost = cost;
+            this.start = start;
+        }
+    }
+}
