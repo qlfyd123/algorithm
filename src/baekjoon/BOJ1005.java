9e61ccc8d16dc556dc6718d1a805942752a50fc3
qlfyd123 <hajewoong@gmail.com>
2023-03-17 09:29:44 +0900

    ACM craft

--- src/baekjoon/BOJ1005.java
+++ src/baekjoon/BOJ1005.java
@@ -3,11 +3,12 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
+/**
+ * <a href="https://www.acmicpc.net/problem/1005">BOJ1005 ACM CRAFT</a>
+ */
+public class BOJ1005 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
@@ -23,52 +24,59 @@ public class Main {
                 rules.add(new ArrayList<>());
             }
             rules.add(new ArrayList<>());
-            boolean[] hasRule = new boolean[N + 1];
+            int[] previousNode = new int[N + 1];
             while (K-- > 0) {
                 st = new StringTokenizer(br.readLine(), " ");
                 int pre = Integer.parseInt(st.nextToken());
                 int next = Integer.parseInt(st.nextToken());
                 rules.get(pre).add(next);
-                hasRule[next] = true;
+                previousNode[next]++;
             }
             int target = Integer.parseInt(br.readLine());
-            ACM acm = new ACM(N, times, rules, target);
-            int BuildTime = 0;
+            ACM acm = new ACM(times, rules, target, previousNode);
+            Queue<Integer> nodes = new LinkedList<>();
             for (int i = 1; i <= N; i++) {
-                if (!hasRule[i]) {
-                    acm.buildTime[i] = times[i];
-                    craft(i, acm);
-                    BuildTime = Math.max(BuildTime, acm.buildTime[i]);
+                if (previousNode[i] == 0) {
+                    nodes.add(i);
                 }
             }
-            System.out.println(BuildTime);
+            while (!nodes.isEmpty()) {
+                int node = nodes.poll();
+                craft(node, acm, nodes);
+            }
         }
     }
 
     static class ACM {
         private final int target;
+        private final int[] previousNode;
         int[] times;
         List<ArrayList<Integer>> rules;
         int[] buildTime;
 
-        public ACM(int N, int[] times, List<ArrayList<Integer>> rules, int target) {
-            buildTime = new int[N + 1];
-            buildTime[target] = times[target];
+        public ACM(int[] times, List<ArrayList<Integer>> rules, int target, int[] previousNode) {
+            buildTime = times.clone();
+            this.previousNode = previousNode;
             this.times = times;
             this.rules = rules;
             this.target = target;
         }
     }
 
-    private static void craft(int buildIndex, ACM acm) {
+    private static void craft(int buildIndex, ACM acm, Queue<Integer> nodes) {
         if (buildIndex == acm.target) {
-            return;
+            System.out.println(acm.buildTime[buildIndex]);
+        } else {
+            for (int next : acm.rules.get(buildIndex)) {
+                int nextBuildTime = acm.times[next] + acm.buildTime[buildIndex];
+                if (acm.buildTime[next] < nextBuildTime) {
+                    acm.buildTime[next] = nextBuildTime;
+                }
+                acm.previousNode[next]--;
+                if (acm.previousNode[next] == 0) {
+                    nodes.add(next);
+                }
+            }
         }
-        int minBuildTime = 0;
-        for (int next : acm.rules.get(buildIndex)) {
-            craft(next, acm);
-            minBuildTime = Math.max(minBuildTime, acm.buildTime[next]);
-        }
-        acm.buildTime[buildIndex] = acm.times[buildIndex] + minBuildTime;
     }
 }
