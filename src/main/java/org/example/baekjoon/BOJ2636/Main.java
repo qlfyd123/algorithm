f78c17a4fd3d9bdbcd4f29c25ca20f4741a44fee
qlfyd123 <86708481+qlfyd123@users.noreply.github.com>
2025-09-15 15:25:19 +0900

    BOJ2636 G4 치즈
    Signed-off-by: qlfyd123 <jw.ga@itconv.co.kr>

--- src/main/java/org/example/baekjoon/BOJ2636/Main.java
+++ src/main/java/org/example/baekjoon/BOJ2636/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new  StringTokenizer(br.readLine(), " ");
         int r, c;
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
